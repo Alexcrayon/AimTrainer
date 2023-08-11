fe014a46f1b9ad928cfea7ff84b4b05be0dedde1
Alex Cao <caoyifei.sz@gmail.com>
2023-08-11 11:13:27 -0600

    Added collisions check between target and mouse clicks - target will be removed on clicks

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ WIDTH = 800
 HEIGHT = 600
 
 WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Aim Trainter")
+pygame.display.set_caption("Aim Trainer")
 
 TARGET_INCREMENT = 400
 TARGET_EVENT = pygame.USEREVENT
@@ -21,17 +21,18 @@ class Target:
     GROWTH_RATE = 0.2
     COLOR = "blue"
     COLOR2 = "white"
-
+    
     def __init__(self, posX, posY):
         self.x = posX
         self.y = posY
         self.size = 0
         self.growing = True
 
+    # update the size the target by growth rate
+    # it is either growing or shrinking
     def update(self):
         if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
             self.growing = False
-
         if self.growing:
             self.size += self.GROWTH_RATE
         else:
@@ -42,9 +43,18 @@ class Target:
         pygame.draw.circle(window, self.COLOR2, (self.x, self.y), self.size * 0.8)
         pygame.draw.circle(window, self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(window, self.COLOR2, (self.x, self.y), self.size * 0.4)
+    
+     
+    # Check collision with mouse clicks
+    def collide(self, posX, posY):
+        distance = math.sqrt((self.x - posX)**2 + (self.y - posY)**2)
+        return distance <= self.size
+
+
 
 # called every frame
-def draw(win, targets):
+# draw and update everything in the main window
+def drawMain(win, targets):
     win.fill(BG_COLOR)
 
     for target in targets:
@@ -61,29 +71,50 @@ def main():
     targets = []
     clock = pygame.time.Clock()
     
+    targetClicked = 0
+    clicks = 0
+    misses = 0
+    startTime = time.time()
+
     # every increment redraw targets on the screen
     pygame.time.set_timer(TARGET_EVENT,TARGET_INCREMENT)
 
-
+    # main game loop
     while running:
         clock.tick(60)
-
+        mouseClicked = False
+        mousePos = pygame.mouse.get_pos()
+        # event listener
         for event in pygame.event.get():
+            # quit game event
             if event.type == pygame.QUIT:
                 running = False
                 break
+            # target generation event
             if event.type == TARGET_EVENT:
                 x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
                 y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
                 newTarget = Target(x,y)
                 targets.append(newTarget)
+            # click event
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseClicked = True
+                clicks += 1
 
+        # update the target list by 
+        # whether a target has been clicked or shrinks to zero size
         for target in targets:
             target.update()
             if target.size <= 0:
                 targets.remove(target)
+                misses += 1
 
-        draw(WINDOW, targets)
+            if mouseClicked and target.collide(*mousePos):
+                targets.remove(target)
+                targetClicked += 1
+
+        # redraw everything
+        drawMain(WINDOW, targets)
 
     pygame.quit()
 
